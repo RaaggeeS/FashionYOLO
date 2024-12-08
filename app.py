21de96af2a04747b12e892d0667ec8a65b3a8092
Raaggee <56103314+RaaggeeS@users.noreply.github.com>
2024-12-08 16:21:05 +0530

    uploaded app.py

--- app.py
+++ app.py
@@ -2,30 +2,55 @@ import streamlit as st
 import matplotlib.pyplot as plt
 from ultralytics import YOLO
 from PIL import Image
+import cv2 as cv
+import tempfile
 
 st.header("Fashion _YOLO_ Classification")
 
 st.write("""This model can classify the following fashion products:""")
-st.write("""Shirt, Jeans, Tshirt, Tops, Socks, Briefs and many more...""")
+st.write("""Cardigans, Denims, Shirts and many more...""")
 
-uploaded_file = st.file_uploader("Choose a file to upload:", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Choose a file to upload:", type=["jpg", "jpeg", "png", "mp4"])
 
+model_trained = YOLO("newbest.pt")
 
+# if uploaded_file is not None:
+#     image = Image.open(uploaded_file)
+#     st.image(uploaded_file, caption="Uploaded Image") 
+#     results = model_trained(image)
 
-model_trained = YOLO("results/runs/classify/train/weights/best.pt")
-
-
+#     for result in results:
+#         top1 = result.probs.top1
+#         classes = result.names
+#         top1_class_name = classes[top1]
+#         print(top1_class_name)
+#         st.write(f"Result is: {top1_class_name}")
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(uploaded_file, caption="Uploaded Image") 
-    results = model_trained(image)
+    preds = set()
+    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".mp4").name
+    
+    with open(temp_file, "wb") as f:
+        f.write(uploaded_file.read())
 
-    for result in results:
-        top1 = result.probs.top1
-        classes = result.names
-        top1_class_name = classes[top1]
-        print(top1_class_name)
-        st.write(f"Result is: {top1_class_name}")
+    st.video(temp_file)
+    video = cv.VideoCapture(temp_file)
 
+    while video.isOpened():
+        ret, frame = video.read()
 
+        if not ret:
+            break
+
+        resized_frames = cv.resize(frame, (224, 224))
+
+        results = model_trained(resized_frames)
+
+        for result in results:
+            top1 = result.probs.top1
+            classes = result.names
+            top1_class_name = classes[top1]
+            if top1 > 0.5:
+                preds.add(top1_class_name)
+
+    st.write(f"Tags are:{preds}")
